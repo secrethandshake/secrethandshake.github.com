61e291a9341e0ec484456014e9627d3e4380cdc5
Christopher Lobay <hello@christopherlobay.com>
2013-03-09 13:07:47 -0600

    OMG MOAR FSVG FICESX

--- server.rb
+++ server.rb
@@ -1,9 +1,15 @@
 require 'webrick'
 include WEBrick
-WEBrick::HTTPUtils::DefaultMimeTypes.store 'svg', 'image/svg+xml'
+
+mime_types = WEBrick::HTTPUtils::DefaultMimeTypes
+mime_types.store 'js', 'application/javascript'
+mime_types.store 'svg', 'image/svg+xml'
+
 s = HTTPServer.new(
  :Port => 3000,
- :DocumentRoot => Dir::pwd
+ :DocumentRoot => Dir::pwd,
+ :MimeTypes => mime_types
 )
+
 trap("INT"){ s.shutdown }
 s.start
